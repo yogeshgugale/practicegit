8403cc14f8c5a448d129189e26488a39256a4736
Yogesh <ugale5432@gmail.com>
2022-12-16 21:48:34 +0530

    first commit

---
+++ src/github_practice1/git1.java
@@ -0,0 +1,17 @@
+package github_practice1;
+
+public class git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HII IM YOGESH");
+		
+		
+		
+		System.out.println("HII GOOD EVEN");
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module github_practice {
+	
+	
+	
+}
